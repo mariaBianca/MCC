baeeafa223f701d1fba34b6841127eda80532ffe
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2016-05-18 11:21:16 +0200

    added a comment

--- app/src/main/java/com/mccapp/bianca/mcc/VideoThing.java
+++ app/src/main/java/com/mccapp/bianca/mcc/VideoThing.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
  * Created by group 8 on 2016-05-17.
  */
 
+// tests
+
 public class VideoThing extends Activity {
 /**    private static VideoThing ourInstance = new VideoThing();
 
