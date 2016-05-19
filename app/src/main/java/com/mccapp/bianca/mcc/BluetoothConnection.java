0ff90695d3ac132f857654a812529761a051de9b
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2016-05-19 10:16:19 +0200

    added labels

--- app/src/main/java/com/mccapp/bianca/mcc/BluetoothConnection.java
+++ app/src/main/java/com/mccapp/bianca/mcc/BluetoothConnection.java
@@ -7,14 +7,12 @@ package com.mccapp.bianca.mcc;
  * This class represents the implementation the bluetooth connection.
  * */
 
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Handler;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
