6bfd04c1c322532eaa37c9b46af5f8289cc94749
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2016-05-29 04:52:52 +0200

    changes that this stuff is asking me to commit

--- app/src/main/java/com/mccapp/bianca/mcc/HomeScreen.java
+++ app/src/main/java/com/mccapp/bianca/mcc/HomeScreen.java
@@ -1,26 +1,24 @@
 package com.mccapp.bianca.mcc;
 
 /**
- * @author Group Ella.
+ *  @author Group Ella.
  */
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 
 /**
  * This class represents the implementation of the HomeScreen view.
- */
+ * */
 
 public class HomeScreen extends AppCompatActivity {
 
     BluetoothConnection bc;
 
     @Override
-    //Beginning of the activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
@@ -30,36 +28,27 @@ public class HomeScreen extends AppCompatActivity {
         buttonButtonView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this, ButtonView.class);
-                startActivity(intent);
-            }
-        });
+                            Intent intent = new Intent(HomeScreen.this, ButtonView.class);
+                            startActivity(intent);
+                                     }
+                   });
 
         //button declaration for JoystickView activity
         Button buttonJoystickView = (Button) this.findViewById(R.id.btnJoystick);
-        buttonJoystickView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this, JoystickControl.class);
-                startActivity(intent);
-            }
+        buttonJoystickView.setOnClickListener(new View.OnClickListener(){
+             @Override
+            public void onClick(View view){
+                 Intent intent = new Intent(HomeScreen.this, JoystickControl.class);
+                 startActivity(intent);
+             }
         });
 
         //button declaration for  episode display activity
         Button buttonEpisode = (Button) this.findViewById(R.id.btnEpisode);
-        buttonEpisode.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this, Episode.class);
-                startActivity(intent);
-            }
-        });
-
 
         //button declaration for the bluetooth connection
         Button buttonBluetooth = (Button) this.findViewById(R.id.btnBluetooth);
         Listeners.BluetoothListener(buttonBluetooth);
-
-        //Button video = (Button) this.findViewById(R.id.videoview);
 
 
     }
