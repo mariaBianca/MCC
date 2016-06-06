fa12ea5009a7564b85cab308cdf1edbd64aa1735
shiwwwei <guslishe@student.gu.se>
2016-06-06 09:45:40 +0200

    Comments addedthe

--- Arduino/ultrasonicsTest.ino
+++ Arduino/ultrasonicsTest.ino
@@ -1,31 +1,40 @@
-//@author Group Ella
+/**
+ *@author Group Ella
+*/
+/**
+ *This class represents the implementation of both forward and backward collision check
+ * by using ultrasonic sensors
+*/  
 
-//importation of the Smartcar library
+//importing Smartcar library
 #include <Smartcar.h>
-//definition of the trigger, echo pins for the 2 ultrasonics
+//defining of the trigger, echo pins for the 2 ultrasonic sensors
 #define trigPin1 5
 #define echoPin1 6
 #define trigPin2 9
 #define echoPin2 10
 
-//declarations used for calculating the distances for the 2 ultrasonic sensors
-//in order to implement the collision check feature
+//declaring calculation of the distances for the 2 ultrasonic sensors
+//implementing the collision check feature
 long duration, distance,BackSensor,FrontSensor;
 
-//declaration of the components of the car
+//declaring car components
 Car car;
 Odometer encoderLeft, encoderRight;
 Gyroscope gyro;
 
-//variables used for the movement of the car
+//declaring variables for the car movement
 int frontSpeed = 40;
 int backSpeed = -40;
 int lDegrees = -70;
 int rDegrees = -70;
 
-//declaration of the buzzer's pin
+//declaring of the buzzer's pin
 int buzzer = 0;
 
+/**
+ * This method implements the car components connections and starting up
+*/
 void setup()
 {
 Serial.begin (9600);
@@ -39,7 +48,9 @@ gyro.begin();
 car.begin(encoderLeft, encoderRight, gyro);
 pinMode(buzzer, 0);
 }
-
+/**
+ * This methods implements looping for distacne calculation for two ultrasonic sensors
+*/ 
 void loop() {
   
 SonarSensor(trigPin1, echoPin1);
@@ -58,7 +69,9 @@ Serial.println(' ');
 distance = 0;
 duration = 0;
 }
-
+/**
+ * This method implements the distance calculation of two ultrasonic sensors
+*/
 void SonarSensor(int trigPin,int echoPin)
 {
 digitalWrite(trigPin, LOW);
